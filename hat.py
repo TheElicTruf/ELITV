0388ce43b0f4c4b140c696f438dfed49e39e2a56
TheElicTruf <elic95@hotmail.it>
2025-05-23 22:19:32 +0200

    Update hat.py

--- hat.py
+++ hat.py
@@ -10,8 +10,8 @@ import time
 import html
 from urllib.parse import urljoin
 
-MFPLINK = "LINKMFP"
-MFPPSW = "MFPPSW"
+MFPLINK = "https://theelian-eligm.hf.space"
+MFPPSW = "Sentenzia"
 # Funzioni dal tuo mpd_decoder.py
 def extract_mpd_link_from_page(url):
     """Estrae il link MPD da una pagina HTML che contiene un iframe con player.html#"""
